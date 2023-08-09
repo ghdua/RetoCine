5dec6e2d657f7fd0e9f8fc0f55f1d699b4e8c7f4
ghdua <daxd69@hotmail.com>
2023-08-09 04:50:54 +0000

    Diseño y desarrollo  de vista splash

--- app/src/main/java/com/wanted/retocine/View/SplashActivity.kt
+++ app/src/main/java/com/wanted/retocine/View/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.wanted.retocine.View
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.wanted.retocine.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar!!.hide()
+
+        // Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
     }
 }
