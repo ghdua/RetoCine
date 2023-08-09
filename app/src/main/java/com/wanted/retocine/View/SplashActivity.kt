84cddeed4930c78bff484df93f2e138d12084603
ghdua <daxd69@hotmail.com>
2023-08-09 03:12:47 +0000

    RetrofitHelper Object, creación de vista de arrance de app: SplashActivity

---
+++ app/src/main/java/com/wanted/retocine/View/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.wanted.retocine.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wanted.retocine.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
