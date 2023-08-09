feb972451e37a30b845169baa629cb7a0b34b56a
ghdua <daxd69@hotmail.com>
2023-08-09 05:36:23 +0000

    Configurando Vista Splash y conexión a Internet

--- app/src/main/java/com/wanted/retocine/View/SplashActivity.kt
+++ app/src/main/java/com/wanted/retocine/View/SplashActivity.kt
@@ -1,10 +1,13 @@
 package com.wanted.retocine.View
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.Observer
+import com.wanted.retocine.LiveData.InternetConnection
 import com.wanted.retocine.R
 
 class SplashActivity : AppCompatActivity() {
@@ -12,12 +15,41 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar!!.hide()
+        CheckInternetConnection()
+    }
 
+    private fun Splash() {
         // Handler().postDelayed({
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000) // 3000 is the delayed time in milliseconds.
+        }, 3000) // 3000 is the delayed time in milliseconds.TODO("Not yet implemented")
+    }
+
+    private fun CheckInternetConnection() {
+        val InternetConnectionObject = InternetConnection(this)
+        InternetConnectionObject.observe(this, Observer
+        { IsConnected ->
+            if (!IsConnected)
+                ShowNoInternetConnection()
+            else Splash()
+        })
+    }
+
+    fun ShowNoInternetConnection()
+    {
+        var PDNoInternetConnection: ProgressDialog = ProgressDialog(this)
+        PDNoInternetConnection.setCancelable(false)
+        PDNoInternetConnection.setIcon(R.drawable.disconnectednetwork)
+        PDNoInternetConnection.setTitle(getString(R.string.NoInternetTitleSpa))
+        PDNoInternetConnection.setMessage(getString(R.string.NoInternetMessageSpa))
+        val NetworkConnectionObject = InternetConnection(this)
+        NetworkConnectionObject.observe(this, Observer { IsConnected ->
+            if (IsConnected)
+                PDNoInternetConnection.dismiss()
+            else
+                PDNoInternetConnection.show()
+        })
     }
 }
